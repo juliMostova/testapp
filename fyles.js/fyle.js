13541be08b3c2e83ee4cc4bf0dcc4ce008d218e0
Juli Mostova <julia.mostova89@gmail.com>
2022-04-26 16:22:29 +0300

    save code

---
+++ fyles.js/fyle.js
@@ -0,0 +1,12 @@
+function createPlayer(name){
+    let goal =0;
+    return function goalCount(){
+        goal++;
+        return `${name}- ${goal} очков !`
+    }
+}
+const playerOne = createPlayer('Денис');
+const playerTwo = createPlayer('Артем');
+
+console.log(playerOne);
+console.log(playerTwo);
