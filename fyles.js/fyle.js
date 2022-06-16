4683066ec9b203e35c33e757127de8136580384d
Juli Mostova <julia.mostova89@gmail.com>
2022-06-16 11:31:03 +0300

    create newFyle

--- fyles.js/fyle.js
+++ fyles.js/fyle.js
@@ -1,17 +1,21 @@
-function getMathResult (num,time) { debugger
-   if( typeof(time) !== 'number' && time <= 0) { debugger
-return num ;debugger
-   } 
-   let str = '';debugger
-
-   for( let i = 1;i <= time; i++){ debugger
-    if( i === time){ debugger
-        str += `${num *i}`; debugger
-    } else {
-        str += `${num *i}---` ;debugger
+ function getMathResult(num, times) { debugger
+    if (typeof(times) !== 'number' || times <= 0) { debugger
+        return num; debugger
     }
-    
-   }
-   return str ;debugger
-}
-console.log(getMathResult(10,5));debugger
+
+    let str = ''; debugger
+
+    for (let i = 1; i <= times; i++) { debugger
+        if (i === times) { debugger
+            str += `${num * i}`; debugger
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`; debugger
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str; debugger
+} 
+getMathResult(10, 5);
